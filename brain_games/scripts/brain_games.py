e5a5e0ec521230ba2c2aed4202d3fa486ab37a06
Roman <flexomj@gmail.com>
2022-11-16 13:10:30 +0400

    add prompt. add name request

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3.
 
+import prompt 
+
 def main():
     print("Welcome to the Brain Games!")
 
-
 if __name__ == '__main__':
     main()
+
+from brain_games.cli import welcome_user
+
+welcome_user()
+
