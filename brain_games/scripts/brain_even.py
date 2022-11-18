8695eb02179fda8ac0b6efcf53491fcd0d66d12e
Roman <flexomj@gmail.com>
2022-11-18 10:58:19 +0400

    fixed by pep8

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,29 +1,35 @@
-
-from brain_games.scripts.brain_games import welcome
-
 from random import randint
 
-welcome()
+
+import prompt
+
 
 def main():
+    print("Welcome to the Brain Games!")
+    name = prompt.string('May I have your name?')
+    print(f'Hello, {name}!')
     print('Answer "yes" if the number is even, otherwise answer "no".')
     i = 1
     while i <= 3:
         question = randint(1, 100)
         print(f'Questions: {question}')
         user_answer = input('Your answer:')
-        if (question % 2 == 0 and user_answer == 'yes') or (question % 2 != 0 and user_answer == 'no'):
+        if (question % 2 == 0 and user_answer == 'yes') or \
+                (question % 2 != 0 and user_answer == 'no'):
             print('Correct!')
             i += 1
         elif question % 2 == 0 and user_answer == 'no':
-            print(f"'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, {name}!")
+            print(f"'no' is wrong answer ;(. Correct answer was 'yes'."
+                  f"\nLet's try again, {name}!")
             exit()
         elif question % 2 != 0 and user_answer == 'yes':
-            print(f"'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, {name}!")
+            print(f"'yes' is wrong answer ;(. Correct answer was 'no'."
+                  f"\nLet's try again, {name}!")
             exit()
         else:
             exit()
     print(f'Congratulations, {name}!')
+
 
 if __name__ == '__main__':
     main()
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -7,6 +7,6 @@ def welcome():
     print("Welcome to the Brain Games!")
     welcome_user()
 
+
 if __name__ == '__main__':
     welcome()
-
