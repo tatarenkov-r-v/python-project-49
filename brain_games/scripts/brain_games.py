e5f6125a4c0868ff85233884285d11ab55161add
Roman <flexomj@gmail.com>
2022-11-18 22:56:21 +0400

    update

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -2,11 +2,10 @@
 
 from brain_games.cli import welcome_user
 
-
-def welcome():
+def main():
     print("Welcome to the Brain Games!")
     welcome_user()
 
 
 if __name__ == '__main__':
-    welcome()
+    main()
